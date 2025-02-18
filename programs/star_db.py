11fb9062c664803f926d5dc274ab98726231dab2
Nao Suzuki <nao.suzuki@gmail.com>
2025-02-18 15:00:01 -0500

    update

--- programs/star_db.py
+++ programs/star_db.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 
 # This is a program for blackbody SED fit
-# Jan 1st  2025 : Note Monday, Feb 17th 2025
+# Jan 1st  2025 : Note Tuesday, Feb 18th 2025
 
 # Dec 10th 2024 : Revision on github
 # Written by Nao Suzuki in November 2024
