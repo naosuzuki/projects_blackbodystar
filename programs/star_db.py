6d5f2646b5007947a2053d9d8b078547829502b4
Nao Suzuki <nao.suzuki@gmail.com>
2025-03-24 10:12:12 -0400

    update

--- programs/star_db.py
+++ programs/star_db.py
@@ -17,9 +17,9 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 
 # This is a program for blackbody SED fit
-# Jan 1st  2025 : Note Mon, Mar 24th 2025
+# Jan 1st  2025 :  Note Mon, Mar 24th 2025
 
-# Dec 10th 2024 : Revision on github
+# Dec 10th 2024 :  Revision on github
 # Written by Nao Suzuki in November 2024
 
 # Speed of Light in cgs
